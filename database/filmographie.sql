2f10839e03991cecfffc467b2f90489e85584a36
root <root@vbox.uha.fr>
2026-06-03 10:51:28 +0200

    Ajout affiches films et username commentaires

--- database/filmographie.sql
+++ database/filmographie.sql
@@ -55,6 +55,220 @@ COMMIT;
 SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
 
 --
+-- Table structure for table `auth_group`
+--
+
+DROP TABLE IF EXISTS `auth_group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth_group`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `auth_group` WRITE;
+/*!40000 ALTER TABLE `auth_group` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_group` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+
+--
+-- Table structure for table `auth_group_permissions`
+--
+
+DROP TABLE IF EXISTS `auth_group_permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_group_permissions` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_group_permissions_group_id_permission_id_0cd325b0_uniq` (`group_id`,`permission_id`),
+  KEY `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` (`permission_id`),
+  CONSTRAINT `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  CONSTRAINT `auth_group_permissions_group_id_b120cbf9_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth_group_permissions`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `auth_group_permissions` WRITE;
+/*!40000 ALTER TABLE `auth_group_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_group_permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+
+--
+-- Table structure for table `auth_permission`
+--
+
+DROP TABLE IF EXISTS `auth_permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `content_type_id` int(11) NOT NULL,
+  `codename` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`),
+  CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth_permission`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `auth_permission` WRITE;
+/*!40000 ALTER TABLE `auth_permission` DISABLE KEYS */;
+INSERT INTO `auth_permission` VALUES
+(1,'Can add log entry',1,'add_logentry'),
+(2,'Can change log entry',1,'change_logentry'),
+(3,'Can delete log entry',1,'delete_logentry'),
+(4,'Can view log entry',1,'view_logentry'),
+(5,'Can add permission',3,'add_permission'),
+(6,'Can change permission',3,'change_permission'),
+(7,'Can delete permission',3,'delete_permission'),
+(8,'Can view permission',3,'view_permission'),
+(9,'Can add group',2,'add_group'),
+(10,'Can change group',2,'change_group'),
+(11,'Can delete group',2,'delete_group'),
+(12,'Can view group',2,'view_group'),
+(13,'Can add user',4,'add_user'),
+(14,'Can change user',4,'change_user'),
+(15,'Can delete user',4,'delete_user'),
+(16,'Can view user',4,'view_user'),
+(17,'Can add content type',5,'add_contenttype'),
+(18,'Can change content type',5,'change_contenttype'),
+(19,'Can delete content type',5,'delete_contenttype'),
+(20,'Can view content type',5,'view_contenttype'),
+(21,'Can add session',6,'add_session'),
+(22,'Can change session',6,'change_session'),
+(23,'Can delete session',6,'delete_session'),
+(24,'Can view session',6,'view_session');
+/*!40000 ALTER TABLE `auth_permission` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+
+--
+-- Table structure for table `auth_user`
+--
+
+DROP TABLE IF EXISTS `auth_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `password` varchar(128) NOT NULL,
+  `last_login` datetime(6) DEFAULT NULL,
+  `is_superuser` tinyint(1) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `first_name` varchar(150) NOT NULL,
+  `last_name` varchar(150) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `is_staff` tinyint(1) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `date_joined` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth_user`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `auth_user` WRITE;
+/*!40000 ALTER TABLE `auth_user` DISABLE KEYS */;
+INSERT INTO `auth_user` VALUES
+(1,'pbkdf2_sha256$1200000$onBBJhQmNk86HUQmLGAGod$j2ytZywnbpzKhq0K3LSRiGiSLgVhsWsxwnKO2cKcwEI=','2026-06-03 08:21:32.623775',1,'admin','','','',1,1,'2026-06-03 08:10:53.941449'),
+(2,'pbkdf2_sha256$1200000$JZlPHDkDvmKzYkCcMAoOUi$Z/jIKdooP0iM37CWcXXVOnY3TdcBMFbXfc8uUVD3yGs=',NULL,0,'demo','','','',0,1,'2026-06-03 08:49:39.671944');
+/*!40000 ALTER TABLE `auth_user` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+
+--
+-- Table structure for table `auth_user_groups`
+--
+
+DROP TABLE IF EXISTS `auth_user_groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_user_groups` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_user_groups_user_id_group_id_94350c0c_uniq` (`user_id`,`group_id`),
+  KEY `auth_user_groups_group_id_97559544_fk_auth_group_id` (`group_id`),
+  CONSTRAINT `auth_user_groups_group_id_97559544_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
+  CONSTRAINT `auth_user_groups_user_id_6a12ed8b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth_user_groups`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `auth_user_groups` WRITE;
+/*!40000 ALTER TABLE `auth_user_groups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_user_groups` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+
+--
+-- Table structure for table `auth_user_user_permissions`
+--
+
+DROP TABLE IF EXISTS `auth_user_user_permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `auth_user_user_permissions` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_user_user_permissions_user_id_permission_id_14a6b632_uniq` (`user_id`,`permission_id`),
+  KEY `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` (`permission_id`),
+  CONSTRAINT `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  CONSTRAINT `auth_user_user_permissions_user_id_a95ead1b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `auth_user_user_permissions`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `auth_user_user_permissions` WRITE;
+/*!40000 ALTER TABLE `auth_user_user_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `auth_user_user_permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+
+--
 -- Table structure for table `categorie`
 --
 
@@ -104,6 +318,7 @@ CREATE TABLE `commentaire` (
   `note` int(11) NOT NULL,
   `commentaire` text DEFAULT NULL,
   `date_commentaire` date NOT NULL,
+  `username` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`id_commentaire`),
   KEY `id_film` (`id_film`),
   KEY `id_personne` (`id_personne`),
@@ -120,15 +335,163 @@ SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
 LOCK TABLES `commentaire` WRITE;
 /*!40000 ALTER TABLE `commentaire` DISABLE KEYS */;
 INSERT INTO `commentaire` VALUES
-(1,1,1,9,'Tres bon film avec un scenario complexe.','2026-05-01'),
-(2,1,2,8,'Film interessant et bien realise.','2026-05-02'),
-(3,2,2,7,'Bon film d action avec de bonnes scenes.','2026-05-03'),
-(4,3,3,10,'Chef oeuvre de science fiction.','2026-05-04'),
-(5,4,4,9,'Film impressionnant visuellement.','2026-05-05'),
-(6,5,5,10,'Excellent film dramatique.','2026-05-06'),
-(7,6,6,8,'Tres bon film de super heros.','2026-05-07'),
-(8,7,7,7,'Film interessant mais un peu long.','2026-05-08');
+(1,1,1,9,'Tres bon film avec un scenario complexe.','2026-05-01',NULL),
+(2,1,2,8,'Film interessant et bien realise.','2026-05-02',NULL),
+(3,2,2,7,'Bon film d action avec de bonnes scenes.','2026-05-03',NULL),
+(4,3,3,10,'Chef oeuvre de science fiction.','2026-05-04',NULL),
+(5,4,4,9,'Film impressionnant visuellement.','2026-05-05',NULL),
+(6,5,5,10,'Excellent film dramatique.','2026-05-06',NULL),
+(7,6,6,8,'Tres bon film de super heros.','2026-05-07',NULL),
+(8,7,7,7,'Film interessant mais un peu long.','2026-05-08',NULL);
 /*!40000 ALTER TABLE `commentaire` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+
+--
+-- Table structure for table `django_admin_log`
+--
+
+DROP TABLE IF EXISTS `django_admin_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `django_admin_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `action_time` datetime(6) NOT NULL,
+  `object_id` longtext DEFAULT NULL,
+  `object_repr` varchar(200) NOT NULL,
+  `action_flag` smallint(5) unsigned NOT NULL CHECK (`action_flag` >= 0),
+  `change_message` longtext NOT NULL,
+  `content_type_id` int(11) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `django_admin_log_content_type_id_c4bce8eb_fk_django_co` (`content_type_id`),
+  KEY `django_admin_log_user_id_c564eba6_fk_auth_user_id` (`user_id`),
+  CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
+  CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `django_admin_log`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `django_admin_log` WRITE;
+/*!40000 ALTER TABLE `django_admin_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `django_admin_log` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+
+--
+-- Table structure for table `django_content_type`
+--
+
+DROP TABLE IF EXISTS `django_content_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `django_content_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `app_label` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `django_content_type`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `django_content_type` WRITE;
+/*!40000 ALTER TABLE `django_content_type` DISABLE KEYS */;
+INSERT INTO `django_content_type` VALUES
+(1,'admin','logentry'),
+(2,'auth','group'),
+(3,'auth','permission'),
+(4,'auth','user'),
+(5,'contenttypes','contenttype'),
+(6,'sessions','session');
+/*!40000 ALTER TABLE `django_content_type` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+
+--
+-- Table structure for table `django_migrations`
+--
+
+DROP TABLE IF EXISTS `django_migrations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `django_migrations` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `app` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `applied` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `django_migrations`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `django_migrations` WRITE;
+/*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
+INSERT INTO `django_migrations` VALUES
+(1,'contenttypes','0001_initial','2026-06-03 08:10:17.570512'),
+(2,'auth','0001_initial','2026-06-03 08:10:18.355716'),
+(3,'admin','0001_initial','2026-06-03 08:10:18.502067'),
+(4,'admin','0002_logentry_remove_auto_add','2026-06-03 08:10:18.514545'),
+(5,'admin','0003_logentry_add_action_flag_choices','2026-06-03 08:10:18.524566'),
+(6,'contenttypes','0002_remove_content_type_name','2026-06-03 08:10:18.635795'),
+(7,'auth','0002_alter_permission_name_max_length','2026-06-03 08:10:18.712318'),
+(8,'auth','0003_alter_user_email_max_length','2026-06-03 08:10:18.767443'),
+(9,'auth','0004_alter_user_username_opts','2026-06-03 08:10:18.776488'),
+(10,'auth','0005_alter_user_last_login_null','2026-06-03 08:10:18.840987'),
+(11,'auth','0006_require_contenttypes_0002','2026-06-03 08:10:18.845858'),
+(12,'auth','0007_alter_validators_add_error_messages','2026-06-03 08:10:18.860141'),
+(13,'auth','0008_alter_user_username_max_length','2026-06-03 08:10:18.912926'),
+(14,'auth','0009_alter_user_last_name_max_length','2026-06-03 08:10:18.974352'),
+(15,'auth','0010_alter_group_name_max_length','2026-06-03 08:10:19.023665'),
+(16,'auth','0011_update_proxy_permissions','2026-06-03 08:10:19.045630'),
+(17,'auth','0012_alter_user_first_name_max_length','2026-06-03 08:10:19.093488'),
+(18,'sessions','0001_initial','2026-06-03 08:10:19.162254');
+/*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
+UNLOCK TABLES;
+COMMIT;
+SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
+
+--
+-- Table structure for table `django_session`
+--
+
+DROP TABLE IF EXISTS `django_session`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `django_session` (
+  `session_key` varchar(40) NOT NULL,
+  `session_data` longtext NOT NULL,
+  `expire_date` datetime(6) NOT NULL,
+  PRIMARY KEY (`session_key`),
+  KEY `django_session_expire_date_a5c62663` (`expire_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `django_session`
+--
+
+SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
+LOCK TABLES `django_session` WRITE;
+/*!40000 ALTER TABLE `django_session` DISABLE KEYS */;
+INSERT INTO `django_session` VALUES
+('fn8of6tkylur03h8c19jvrz70ksmhcrp','.eJxVjMEOwiAQRP-FsyFAsVs8eu83kN1llaqhSWlPxn-XJj3oaZJ5b-atIm5rjluVJU5JXZRVp9-OkJ9SdpAeWO6z5rmsy0R6V_RBqx7nJK_r4f4dZKy5rd0tdSBMwMZbwkBhoC4YABCDZ7TkWyL2mNhLYOvQNdm74Kg3gwX1-QL_wzhH:1wUghY:xUGpfrY6JrR-t5gMZ3QZHnZVaIFnncYLlqfz9dSZiKo','2026-06-17 08:11:48.406983');
+/*!40000 ALTER TABLE `django_session` ENABLE KEYS */;
 UNLOCK TABLES;
 COMMIT;
 SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
@@ -161,13 +524,13 @@ SET @OLD_AUTOCOMMIT=@@AUTOCOMMIT, @@AUTOCOMMIT=0;
 LOCK TABLES `film` WRITE;
 /*!40000 ALTER TABLE `film` DISABLE KEYS */;
 INSERT INTO `film` VALUES
-(1,'Inception',2010,'inception.jpg','Christopher Nolan',2),
-(2,'Gladiator',2000,'gladiator.jpg','Ridley Scott',1),
-(3,'Interstellar',2014,'interstellar.jpg','Christopher Nolan',2),
-(4,'Avatar',2009,'avatar.jpg','James Cameron',2),
-(5,'Titanic',1997,'titanic.jpg','James Cameron',3),
-(6,'The Dark Knight',2008,'darkknight.jpg','Christopher Nolan',1),
-(7,'Joker',2019,'joker.jpg','Todd Phillips',7),
+(1,'Inception',2010,'https://fr.web.img6.acsta.net/pictures/15/10/14/11/39/161116.jpg','Christopher Nolan',2),
+(2,'Gladiator',2000,'https://fr.web.img6.acsta.net/medias/nmedia/18/68/64/41/19254537.jpg','Ridley Scott',1),
+(3,'Interstellar',2014,'https://fr.web.img6.acsta.net/pictures/14/09/24/12/08/158828.jpg','Christopher Nolan',2),
+(4,'Avatar',2009,'https://fr.web.img6.acsta.net/medias/nmedia/18/78/95/70/19485155.jpg','James Cameron',2),
+(5,'Titanic',1997,'https://fr.web.img6.acsta.net/medias/nmedia/18/36/27/01/18455183.jpg','James Cameron',3),
+(6,'The Dark Knight',2008,'https://fr.web.img6.acsta.net/medias/nmedia/18/63/97/89/18949761.jpg','Christopher Nolan',1),
+(7,'Joker',2019,'https://fr.web.img6.acsta.net/pictures/19/09/03/12/02/4765874.jpg','Todd Phillips',7),
 (8,'Avengers Endgame',2019,'endgame.jpg','Anthony Russo',1),
 (9,'Mad Max Fury Road',2015,'madmax.jpg','George Miller',1),
 (10,'John Wick',2014,'johnwick.jpg','Chad Stahelski',1),
@@ -277,4 +640,4 @@ SET AUTOCOMMIT=@OLD_AUTOCOMMIT;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2026-05-20 14:01:08
+-- Dump completed on 2026-06-03 10:51:28
